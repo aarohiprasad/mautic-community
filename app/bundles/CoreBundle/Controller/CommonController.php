11b048eb29993c175d4e9147227c5180a9cd8cb6
aarohiprasad <aarohi.prasad@acquia.com>
2025-02-19 20:01:25 +0530

    reverted common controller changes

--- app/bundles/CoreBundle/Controller/CommonController.php
+++ app/bundles/CoreBundle/Controller/CommonController.php
@@ -17,7 +17,6 @@ use Mautic\CoreBundle\Model\AbstractCommonModel;
 use Mautic\CoreBundle\Security\Permissions\CorePermissions;
 use Mautic\CoreBundle\Service\FlashBag;
 use Mautic\CoreBundle\Translation\Translator;
-use Mautic\PageBundle\Helper\PageConfig;
 use Mautic\PageBundle\Model\PageModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -52,7 +51,6 @@ class CommonController extends AbstractController implements MauticController
         private FlashBag $flashBag,
         private ?RequestStack $requestStack,
         protected ?CorePermissions $security,
-        protected ?PageConfig $pageConfig,
     ) {
         $this->user = $userHelper->getUser();
     }
